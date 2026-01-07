368419e0ef0d92297ee4658c0df1ff2e48835591
mmoha-199 <mmyacont20@hotmail.com>
2026-01-07 02:19:56 +0000

    Migration to MVVM

--- Notes/App.xaml.cs
+++ Notes/App.xaml.cs
@@ -5,8 +5,9 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+	    Routing.RegisterRoute(nameof(Views.NotePage), typeof(Views.NotePage));
+	    //MainPage = new AppShell();
 	}
-
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
 		return new Window(new AppShell());
